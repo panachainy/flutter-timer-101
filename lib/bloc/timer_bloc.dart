940ea32e9a64f164db556b7fd0cb3cd51220b6ba
Panachai <panachai.ny@gmail.com>
2022-09-19 16:12:46 +0700

    Update timer_bloc.dart

--- lib/bloc/timer_bloc.dart
+++ lib/bloc/timer_bloc.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
 import 'package:flutter_timer/ticker.dart';
 
 part 'timer_event.dart';
@@ -16,6 +17,8 @@ class TimerBloc extends Bloc<TimerEvent, TimerState> {
       : _ticker = ticker,
         super(const TimerInitial(_duration)) {
     on<TimerStarted>(_onStarted);
+    on<TimerPaused>(_onPaused);
+    on<TimerResumed>(_onResumed);
     on<TimerTicked>(_onTicked);
   }
 
@@ -31,6 +34,20 @@ class TimerBloc extends Bloc<TimerEvent, TimerState> {
     _tickerSubscription = _ticker
         .tick(ticks: event.duration)
         .listen((duration) => add(TimerTicked(duration: duration)));
+  }
+
+  void _onPaused(TimerPaused event, Emitter<TimerState> emit) {
+    if (state is TimerRunInProgress) {
+      _tickerSubscription?.pause();
+      emit(TimerRunPause(state.duration));
+    }
+  }
+
+  void _onResumed(TimerResumed resume, Emitter<TimerState> emit) {
+    if (state is TimerRunPause) {
+      _tickerSubscription?.resume();
+      emit(TimerRunInProgress(state.duration));
+    }
   }
 
   void _onTicked(TimerTicked event, Emitter<TimerState> emit) {
