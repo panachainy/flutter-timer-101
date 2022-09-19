a07453696cf3d6dbb323b914eedd093051553c83
Panachai <panachai.ny@gmail.com>
2022-09-19 16:32:17 +0700

    Update timer_page.dart

--- lib/timer/view/timer_page.dart
+++ lib/timer/view/timer_page.dart
@@ -54,3 +54,73 @@ class TimerText extends StatelessWidget {
     );
   }
 }
+
+class Actions extends StatelessWidget {
+  const Actions({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<TimerBloc, TimerState>(
+      buildWhen: (prev, state) => prev.runtimeType != state.runtimeType,
+      builder: (context, state) {
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            if (state is TimerInitial) ...[
+              FloatingActionButton(
+                child: const Icon(Icons.play_arrow),
+                onPressed: () => context
+                    .read<TimerBloc>()
+                    .add(TimerStarted(duration: state.duration)),
+              ),
+            ],
+            if (state is TimerRunInProgress) ...[
+              FloatingActionButton(
+                child: const Icon(Icons.pause),
+                onPressed: () => context.read<TimerBloc>().add(TimerPaused()),
+              ),
+              FloatingActionButton(
+                child: const Icon(Icons.replay),
+                onPressed: () => context.read<TimerBloc>().add(TimerReset()),
+              ),
+            ],
+            if (state is TimerRunPause) ...[
+              FloatingActionButton(
+                child: const Icon(Icons.play_arrow),
+                onPressed: () => context.read<TimerBloc>().add(TimerResumed()),
+              ),
+              FloatingActionButton(
+                child: const Icon(Icons.replay),
+                onPressed: () => context.read<TimerBloc>().add(TimerReset()),
+              ),
+            ],
+            if (state is TimerRunComplete) ...[
+              FloatingActionButton(
+                child: const Icon(Icons.replay),
+                onPressed: () => context.read<TimerBloc>().add(TimerReset()),
+              ),
+            ]
+          ],
+        );
+      },
+    );
+  }
+}
+
+class Background extends StatelessWidget {
+  const Background({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Colors.blue.shade50,
+            Colors.blue.shade500,
+          ],
+        ),
+      ),
+    );
+  }
+}
